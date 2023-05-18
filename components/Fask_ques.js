2860a8763632afdf7f1e1f49d4cadfb71e3cc399
callsmasterofficia <partnerchc@outlook.com>
2023-05-18 13:48:12 +0530

    pipeline

--- components/Fask_ques.js
+++ components/Fask_ques.js
@@ -1,78 +1,70 @@
-import siteConfig from "../siteConfig"
+import siteConfig from '../siteConfig'
 
 function Fask_question() {
   return (
     <>
-
-
       <div className="f-ask-container m-auto mt-14 w-[80%]">
         {/* text section */}
         <div className="f-ask-box  mb-10 flex items-center justify-center border-b-2  border-white">
-          <h4 className="mb-5 p-2 text-2xl md:text-3xl font-semibold primary_text_color">
+          <h4 className="primary_text_color mb-5 p-2 text-2xl font-semibold md:text-3xl">
             Frequently Asked Questions
           </h4>
         </div>
-         <div className="accordion " id="accordionExample" >
-    {siteConfig.faq.accordian.map(item=>(
-    
-    <div className="accordion-item" key={item.id}>
-    <h4 className="accordion-header" id={`heading-${item.id}`}>
-      <button
-        className="
+        <div className="accordion " id="accordionExample">
+          {siteConfig.faq.accordian.map((item) => (
+            <div className="accordion-item" key={item.id}>
+              <h4 className="accordion-header" id={`heading-${item.id}`}>
+                <button
+                  className="
+          secondary
+          collapsed
           accordion-button
           relative
+          mt-2
           flex
           w-full
           items-center
-          rounded-none
-          border-b-2
-          border-white
-          secondary py-4 px-5
+          rounded-none border-b-2 border-white
+          py-4
+          px-5
           text-left
           text-base
           text-white
           transition
           focus:outline-none
-          mt-2
-          collapsed
         "
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target={`#collapse${item.id}`}
-        aria-expanded="true"
-        aria-controls={`collapse${item.id}`}
-        style={{backgroundColor: "black", color: "white"}}
-        >
-        {item.question}
-      </button>
-    </h4>
-    <div
-      id={`collapse${item.id}`}
-      className="collapse accordion-collapse "
-      aria-labelledby={`heading-${item.id}`}
-      data-bs-parent="#accordionExample"
-      style={{backgroundColor: "#303030"}}
-    >
-      <div className="accordion-body py-4 px-5 primary_text_color">
-        {item.answer}
-        <p>{item.p1}</p>
-        <p>{item.p2}</p>
-        <p>{item.p3}</p>
-        <p>{item.p4}</p>
-        <p>{item.p5}</p>
-      </div>
-    </div>
-  </div>
-    ))}
-    
-    
-  </div>
-
-        
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse${item.id}`}
+                  aria-expanded="true"
+                  aria-controls={`collapse${item.id}`}
+                  style={{ backgroundColor: 'black', color: 'white' }}
+                >
+                  {item.question}
+                </button>
+              </h4>
+              <div
+                id={`collapse${item.id}`}
+                className="accordion-collapse collapse "
+                aria-labelledby={`heading-${item.id}`}
+                data-bs-parent="#accordionExample"
+                style={{ backgroundColor: '#303030' }}
+              >
+                <div className="accordion-body primary_text_color py-4 px-5">
+                  {item.answer}
+                  <p>{item.p1}</p>
+                  <p>{item.p2}</p>
+                  <p>{item.p3}</p>
+                  <p>{item.p4}</p>
+                  <p>{item.p5}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
 }
-
 
 export default Fask_question
--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-
 import Head from 'next/head'
-import Navbar from "../components/Navbar" 
-import Banner from "../components/Banner"
-import Text from '../components/Text';
-import Text2 from "../components/Text2"
-import Fask_ques from "../components/Fask_ques"
-import Footer from "../components/Footer"
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Text from '../components/Text'
+import Text2 from '../components/Text2'
+import Fask_ques from '../components/Fask_ques'
+import Footer from '../components/Footer'
 import Script from 'next/script'
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   return {
-    props:{
-      meta:{
-        title:"Netflix Party - obejrzyjmy razem | Zainstaluj rozszerzenie teraz",
-        description:"Netflix Party umożliwia oglądanie serwisu Netflix z przyjaciółmi online. Zainstaluj rozszerzenie, aby zsynchronizować odtwarzanie wideo.",
-        keywords:"Netflix Party, Netflix Watch Party, Netflix Party Extension, Netflix Party Chrome Extension, Watch Party Netflix, How to do Netflix Party, How to use Netflix Party, How to do a Netflix Party, Netflix Watch Party Extension",
-        pageUrl:"https://www.netflixparty.pl/",
-        featuredImage:""
-      }
-    }
+    props: {
+      meta: {
+        title:
+          'Netflix Party - obejrzyjmy razem | Zainstaluj rozszerzenie teraz',
+        description:
+          'Netflix Party umożliwia oglądanie serwisu Netflix z przyjaciółmi online. Zainstaluj rozszerzenie, aby zsynchronizować odtwarzanie wideo.',
+        keywords:
+          'Netflix Party, Netflix Watch Party, Netflix Party Extension, Netflix Party Chrome Extension, Watch Party Netflix, How to do Netflix Party, How to use Netflix Party, How to do a Netflix Party, Netflix Watch Party Extension',
+        pageUrl: 'https://www.netflixparty.pl/',
+        featuredImage: '',
+      },
+    },
   }
 }
-
 
 const Home = () => {
   return (
@@ -38,14 +39,14 @@ const Home = () => {
       </Head>
       <Script src="https://cdn.tailwindcss.com" />
 
-       <Navbar/>
-       <Banner/>
-       <Text/>
-      <Text2/>
-       <Fask_ques/>
-       <Footer/>
-       
-       <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></Script>
+      <Navbar />
+      <Banner />
+      <Text />
+      <Text2 />
+      <Fask_ques />
+      <Footer />
+
+      <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></Script>
     </div>
   )
 }
